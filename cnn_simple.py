889e2f5d2519d8ab3dcf2e7d8bb15b2abed350e2
Piotr Migdał <pmigdal@gmail.com>
2017-11-07 18:21:15 +0100

    cnn simple

--- cnn_simple.py
+++ cnn_simple.py
@@ -37,12 +37,10 @@ model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPool2D())
 
 model.add(Flatten())
-model.add(Dense(256, activation='relu'))
-model.add(Dropout(0.5))
 model.add(Dense(10))
 model.add(Activation('softmax'))
 
-model.compile(optimizer='rmsprop',
+model.compile(optimizer='adam',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
